aa093c14c174c0e34705a86c31d48a4f79148c09
VictorFioravanteXavier <157313401+VictorFioravanteXavier@users.noreply.github.com>
2024-02-23 00:02:39 -0300

    estudos

--- Aulas/Instalacao Angular/meu-primeiro-projeto-latest/src/app/app.component.ts
+++ Aulas/Instalacao Angular/meu-primeiro-projeto-latest/src/app/app.component.ts
@@ -1,15 +1,20 @@
 import { CommonModule, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NewComponent } from './components/new-component/new-component.component';
 
 @Component({
   selector: 'app-root',   
+  styles: [`
+    h1{
+      color: red
+    }
+  `],
   template:`
     <!-- <router-outlet></router-outlet> -->
     <h1> Curso de Angular </h1>
-    <h2> Componentes </h2>
-
-    {{title}}
+    
+    <app-new-component></app-new-component>
   `,
 })
 export class AppComponent {}
---
+++ Aulas/Instalacao Angular/meu-primeiro-projeto-latest/src/app/components/new-component/new-component.component.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-component', 
+  templateUrl: './new-component.component.html',
+  styleUrls: ['./new-component.component.scss']
+})
+export class NewComponent { }
