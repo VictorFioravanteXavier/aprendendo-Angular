4475f2178ffa607b0e343260585cb7bcbe0b0e7b
VictorFioravanteXavier <157313401+VictorFioravanteXavier@users.noreply.github.com>
2024-03-02 23:03:16 -0300

    teste chart

---
+++ Projetos - Testes/angular-chart-example/src/app/chart/chart.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { Chart }from 'chart.js'; // Importar o pacote Chart.js
+
+@Component({
+  selector: 'app-chart',
+  templateUrl: './chart.component.html',
+  styleUrls: [] // Remova a referência ao arquivo CSS
+})
+
+export class ChartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    const ctx = document.getElementById('myChart') as HTMLCanvasElement;
+    const data = [
+      { year: 2010, count: 10 },
+      { year: 2011, count: 20 },
+      { year: 2012, count: 15 },
+      { year: 2013, count: 25 },
+      { year: 2014, count: 22 },
+      { year: 2015, count: 30 },
+      { year: 2016, count: 28 },
+    ];
+
+    new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: data.map(row => row.year),
+        datasets: [
+          {
+            label: 'Acquisitions by year',
+            data: data.map(row => row.count)
+          }
+        ]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
+
+}
