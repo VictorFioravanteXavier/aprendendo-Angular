a452055f302b7f187b55ef0f9b2bba33c0c6c254
VictorFioravanteXavier <157313401+VictorFioravanteXavier@users.noreply.github.com>
2024-03-21 00:30:06 -0300

    estudos

--- V13 até v15/meu-primeiro-projeto/src/app/app.component.ts
+++ V13 até v15/meu-primeiro-projeto/src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
-  <router-outlet></router-outlet>
+    {{valor}}
+    <button (click)='adicionar()'>Adicionar</button>
+    <router-outlet></router-outlet>
   `
 })
-export class AppComponent {
+export class AppComponent implements OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewChecked, AfterViewInit{
+  public valor: number = 1;
+  
+  constructor() {}
+
+  public adicionar () : number {
+    return this.valor += 1;
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  ngDoCheck(): void {
+    console.log("ngDoCheck")
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit')
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked')
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked')
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit')
+  }
+  
 }
--- V13 até v15/meu-primeiro-projeto/src/app/components/title/title.component.ts
+++ V13 até v15/meu-primeiro-projeto/src/app/components/title/title.component.ts
@@ -1,17 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.scss']
 })
-export class TitleComponent implements OnInit {
+export class TitleComponent implements OnInit, OnChanges {
 
-  public title : string = 'Bem Vindo!';
+  @Input() public title : string = 'Bem Vindo!';
   
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(): void {
+    alert('Foi alterado com sucesso')
   }
 
 }
