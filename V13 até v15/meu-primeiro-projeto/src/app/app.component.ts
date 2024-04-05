1cdbcead797281fc4dfac0425490d88567a75782
VictorFioravanteXavier <157313401+VictorFioravanteXavier@users.noreply.github.com>
2024-04-04 22:45:16 -0300

    estudos

--- V13 até v15/meu-primeiro-projeto/src/app/app.component.ts
+++ V13 até v15/meu-primeiro-projeto/src/app/app.component.ts
@@ -3,7 +3,13 @@ import { Component, OnInit} from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <app-data-biding></app-data-biding>
+
+    <!--<app-diretivas-estruturais></app-diretivas-estruturais>-->
+    <!--<app-diretivas-atributos>
+      <h1>Aulas de diretivas</h1>
+      <hr>
+    </app-diretivas-atributos>-->
+    <app-diretivas-atributos></app-diretivas-atributos>
     <router-outlet></router-outlet>
   `
 })
--- V13 até v15/meu-primeiro-projeto/src/app/app.module.ts
+++ V13 até v15/meu-primeiro-projeto/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { TitleComponent } from './components/title/title.component';
 import { DataBidingComponent } from './components/data-biding/data-biding.component';
 import { FormsModule } from '@angular/forms';
+import { DiretivasEstruturaisComponent } from './components/diretivas-estruturais/diretivas-estruturais.component';
+import { DiretivasAtributosComponent } from './components/diretivas-atributos/diretivas-atributos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TitleComponent,
     DataBidingComponent,
+    DiretivasEstruturaisComponent,
+    DiretivasAtributosComponent,
 
   ],
   imports: [
